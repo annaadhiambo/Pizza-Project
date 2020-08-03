c0e90015f7aa12785a9307a9f9a0c7ef24d7b648
annaadhiambo <annaadhiambo01@gmail.com>
2020-08-03 18:48:36 +0300

    added js part

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,28 @@
-// $(document).ready(function(){
-//     $('form#work').submit(function(){
-//         var name = $('input#name').val();
-//         var email = $('input#email').val();
-//         var message = $('input#textarea').val();
-//         if (name && email){
-//             alert (name + ', we have recived your message. Thank you for contacting us ');
-//         }else {
-//             alert('Please enter your name and email');
-//         }
-//     });
-// });
+var price , crust_price, topping_price ;
+let total = 0;
+function Getpizza( name,size,crust,topping){
+  this.name = name;
+  this.size = size;
+  this.crust = crust;
+  this.topping = topping;
+}
+Getpizza.prototype.fullorder=function(){
+    return this.name + ", " +  this.size + ", " + this.crust + ", " + this.topping
+}
+$(document).ready(function(){
+    $("#form").submit(function(event){
+        event.preventDefault();
+        var name =$("#flavour").val();
+        var size =$("#size").val();
+        var crust =$("#crust").val();
+        var topping =$("#topping").val();
+        var number =$("#number").val();
 
+        newpizzaorder =new Getpizza(name,size,crust,topping)
+        alert("Your order is" +", " + newpizzaorder.fullorder())
+
+        
+    });
+
+
+});
