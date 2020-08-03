66708a42a81755f3b32ce1be62a954ccdddb0dde
annaadhiambo <annaadhiambo01@gmail.com>
2020-08-03 12:03:56 +0300

    corrected some errors

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// $(document).ready(function(){
+//     $('form#work').submit(function(){
+//         var name = $('input#name').val();
+//         var email = $('input#email').val();
+//         var message = $('input#textarea').val();
+//         if (name && email){
+//             alert (name + ', we have recived your message. Thank you for contacting us ');
+//         }else {
+//             alert('Please enter your name and email');
+//         }
+//     });
+// });
+
